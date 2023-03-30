2075c9fe94e715798fe5fde00070c3c8a4e94393
cindy-hj <hj0304hj@gmail.com>
2023-03-30 17:52:04 +0900

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import BoardView from '../views/BoardView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  }, {
+    path: '/board',
+    name: 'board',
+    component: BoardView
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/BoardView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h3>게시글 목록</h3>
+        {{ state.row }}
+    </div>
+</template>
+
+<script>
+import { reactive, onMounted } from "vue";
+import axios from "axios";
+
+export default {
+    setup () {
+        const state = reactive({
+            row : [],
+        })
+
+        const handleData = async() => {
+            const url = `/api/boards`;
+            const headers = {"Content-Type":"application/json"};
+            const { data } = await axios.get(url, {headers});
+            console.log(data);
+            
+            // if(data.status === 200) {
+                state.row = data
+            // }
+        };
+
+        onMounted(()=>{
+            handleData();
+        })
+
+        return {
+            state
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
